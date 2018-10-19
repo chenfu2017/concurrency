65bae9dccf1df365647e96c132ced33c19adf219
chenfu <211147056@qq.com>
2018-10-19 10:33:36 +0800

    example

---
+++ src/main/java/com/chenfu/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,49 @@
+package com.chenfu.example.concurrent;
+
+import com.chenfu.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class ConcurrentHashMapExample {
+    public static int threadTotal = 200;
+
+    public static int clientTotal = 5000;
+
+    public static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+
+    public static Map<Integer, Integer> map2 = new ConcurrentSkipListMap<>();
+
+    public static void main(String[] args) throws Exception{
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception:",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("map1:{}",map.size());
+        log.info("map2:{}",map.size());
+    }
+
+    private  static void update(int i) {
+        map.put(i,i);
+        map2.put(i, i);
+    }
+}
---
+++ src/main/java/com/chenfu/example/concurrent/SkipListExample1.java
@@ -0,0 +1,45 @@
+package com.chenfu.example.concurrent;
+
+import com.chenfu.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+@Slf4j
+@ThreadSafe
+public class SkipListExample1 {
+    public static int threadTotal = 200;
+
+    public static int clientTotal = 5000;
+
+    public static Set<Integer> set = new ConcurrentSkipListSet<>();
+
+
+    public static void main(String[] args) throws Exception{
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception:",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println("size:" + set.size());
+    }
+
+    private  static void update(int i) {
+        set.add(i);
+    }
+}
